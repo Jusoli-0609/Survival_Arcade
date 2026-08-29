977cc89cd25ee8a11fd61a6c90a5af893e0d979f
JUSOLI-0609 <sori7700@gmail.com>
2026-08-29 21:15:24 +0900

    HealingItem 구현

---
+++ Source/Survival_Arcade/Public/HealingItem.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseItem.h"
+#include "HealingItem.generated.h"
+
+UCLASS()
+class SURVIVAL_ARCADE_API AHealingItem : public ABaseItem
+{
+	GENERATED_BODY()
+	
+public:
+	AHealingItem();
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+	float HealAmount;
+
+	virtual void ActivateItem(AActor* Activator) override;
+};
