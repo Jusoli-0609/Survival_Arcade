f2761310a676c00077149237ae0f2ba031b9e96e
JUSOLI-0609 <sori7700@gmail.com>
2026-08-31 16:15:12 +0900

    아이템 BP 생성

--- Source/Survival_Arcade/Private/BaseItem.cpp
+++ Source/Survival_Arcade/Private/BaseItem.cpp
@@ -1,9 +1,23 @@
 ﻿#include "BaseItem.h"
+#include "Components/SphereComponent.h"
 
 ABaseItem::ABaseItem()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
+	Scene = CreateDefaultSubobject<USceneComponent>(TEXT("Scene"));
+	SetRootComponent(Scene);
+	// 루트 컴포넌트 생성 및 설정
+
+    Collision = CreateDefaultSubobject<USphereComponent>(TEXT("Collision"));
+	// 충돌 컴포넌트 생성 및 설정
+    Collision->SetCollisionProfileName(TEXT("OverlapAllDynamic"));
+    // 겹침만 감지하는 프로파일 설정
+
+    StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+    // 스태틱 메시 컴포넌트 생성 및 설정
+    StaticMesh->SetupAttachment(Collision);
+		// 메시가 불필요하게 충돌을 막지 않도록 하기 위해, 별도로 NoCollision 등으로 설정할 수 있음.
 }
 
 void ABaseItem::OnItemOverlap(AActor* OverlapActor)
--- Source/Survival_Arcade/Public/BaseItem.h
+++ Source/Survival_Arcade/Public/BaseItem.h
@@ -5,6 +5,8 @@
 #include "ItemInterface.h"
 #include "BaseItem.generated.h"
 
+class USphereComponent;
+
 UCLASS()
 class SURVIVAL_ARCADE_API ABaseItem : public AActor, public IItemInterface
 {
@@ -14,6 +16,19 @@ public:
 	ABaseItem();
 
 protected:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+	FName ItemType;
+	//아이템 이름 같은 경우는 기획자분들이 바꾸는경우가 많기 때문에
+	//뤤만하면 리픅렉션해서 수정 가능하게 만든다.
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item|Component")
+	USceneComponent* Scene;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item|Component")
+	USphereComponent* Collision;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item|Component")
+	UStaticMeshComponent* StaticMesh;
+
 	virtual void OnItemOverlap(AActor* OverlapActor) override;
 	virtual void OnItemEndOverlap(AActor* OverlapActor)override;
 	virtual void ActivateItem(AActor* Activator) override;
@@ -21,10 +36,4 @@ protected:
 	//인터페이스에 있던 것들.
 
 	virtual void DestroyItem();
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
-	FName ItemType;
-	//아이템 이름 같은 경우는 기획자분들이 바꾸는경우가 많기 때문에
-	//뤤만하면 리픅렉션해서 수정 가능하게 만든다.
-
 };
