e0921f907994f4373a5759dbe648b3d74aa9a19f
JUSOLI-0609 <sori7700@gmail.com>
2026-09-02 03:37:52 +0900

    아이템 확률 DT작성

---
+++ Source/Survival_Arcade/Public/ItemSpawnRow.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "ItemSpawnRow.generated.h"
+
+USTRUCT(BlueprintType)
+struct FItemSpawnRow : public FTableRowBase
+	//데이터 테이블 행으로 쓰겠다는 의미
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FName ItemName;
+	//아이템 이름
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSubclassOf<AActor> ItemClass;
+	//클래스 이름
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Spawnchance;
+	//라리템 확률
+};
