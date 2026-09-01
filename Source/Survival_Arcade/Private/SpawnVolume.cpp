84cdebd7b2ce9e9d6c9b7b346347f0f0b579d46a
JUSOLI-0609 <sori7700@gmail.com>
2026-09-01 19:54:40 +0900

    SpawnVolume 생성

---
+++ Source/Survival_Arcade/Private/SpawnVolume.cpp
@@ -0,0 +1,45 @@
+﻿#include "SpawnVolume.h"
+#include "Components/BoxComponent.h"
+#include "Engine/World.h"
+#include "GameFramework/Actor.h"
+
+ASpawnVolume::ASpawnVolume()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+    // 박스 컴포넌트를 생성하고, 이 액터의 루트로 설정
+    Scene = CreateDefaultSubobject<USceneComponent>(TEXT("Scene"));
+    SetRootComponent(Scene);
+
+    SpawningBox = CreateDefaultSubobject<UBoxComponent>(TEXT("SpawningBox"));
+    SpawningBox->SetupAttachment(Scene);
+}
+
+FVector ASpawnVolume::GetRandomPointInVolume() const    //랜덤 좌표를 얻는 함수
+{
+    FVector BoxExtent = SpawningBox->GetScaledBoxExtent();
+    // 1) 박스 컴포넌트의 스케일된 Extent, 즉 x/y/z 방향으로 반지름(절반 길이)을 구함
+    FVector BoxOrigin = SpawningBox->GetComponentLocation();
+    // 2) 박스 중심 위치
+    // 중심 -> 끝
+
+    return BoxOrigin + FVector(
+    // 3) 각 축별로 -Extent ~ +Extent 범위의 무작위 값 생성
+        FMath::FRandRange(-BoxExtent.X, BoxExtent.X),
+        FMath::FRandRange(-BoxExtent.Y, BoxExtent.Y),
+        FMath::FRandRange(-BoxExtent.Z, BoxExtent.Z)
+    );
+}
+
+void ASpawnVolume::SpawnItem(TSubclassOf<AActor> ItemClass)
+{
+    if (!ItemClass) return;
+
+    GetWorld()->SpawnActor<AActor>(
+        ItemClass,
+        GetRandomPointInVolume(),
+        FRotator::ZeroRotator
+    );
+}
+
+
---
+++ Source/Survival_Arcade/Public/SpawnVolume.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SpawnVolume.generated.h"
+
+class UBoxComponent;
+
+UCLASS()
+class SURVIVAL_ARCADE_API ASpawnVolume : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+    ASpawnVolume();
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Spawning")
+    USceneComponent* Scene;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Spawning")
+    UBoxComponent* SpawningBox;
+    // 스폰 영역을 담당할 박스 컴포넌트
+
+    UFUNCTION(BlueprintCallable, Category = "Spawning")
+    FVector GetRandomPointInVolume() const;
+    // 스폰 볼륨 내부에서 무작위 좌표를 얻어오는 함수
+    UFUNCTION(BlueprintCallable, Category = "Spawning")
+    void SpawnItem(TSubclassOf<AActor> ItemClass);
+    // 특정 아이템 클래스를 스폰하는 함수
+
+};
